bd4990b1d11de2c466dc05318d8f5fc8cdcb4312
KC <kevinchen@Kevins-MacBook-Air.local>
2023-01-18 18:13:49 -0500

    update

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:4000/', {
+  const response = await fetch('https://openai-ks1i.onrender.com/' , {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
