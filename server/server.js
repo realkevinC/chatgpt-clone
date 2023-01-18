538dd66472a22fbb89b00c2e3e030a6bf456c211
KC <kevinchen@Kevins-MacBook-Air.local>
2023-01-18 16:54:15 -0500

    server side

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import { Express } from "express";
+import  express  from "express";
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import { Configuration, OpenAIApi } from "openai";
@@ -9,4 +9,39 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Hello from Codex'
+    })
+})
+
+app.post('/', async(req, res) =>{
+    try {
+        const prompt = req.body.prompt;
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,  
+            max_tokens: 4096, 
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+          });
+          res.status(200).send({
+            bot: response.data.choices[0].text
+          })
+    } catch (error){
+        console.log(error);
+        res.status(500).send({error})
+    }
+})
+
+app.listen(4000, () => console.log('AI server started on https://localhost:4000'))
