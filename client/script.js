36ca35fb19a69c07d39d9f84cac1ef16d6fbd3e9
KC <kevinchen@Kevins-MacBook-Air.local>
2023-01-18 18:47:47 -0500

    deploy

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('https://openai-sfw3.onrender.com/' , {
+  const response = await fetch('https://openai-sfw3.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
